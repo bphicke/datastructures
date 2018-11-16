fbd3dcc56e4cc360ae4ea344c39ac3ee49ec6eed
brian <bphicke@gmail.com>
2018-11-15 20:45:10 -0600

    Add pop method to doubly linked list

--- DLL-Classes.js
+++ DLL-Classes.js
@@ -12,5 +12,33 @@ class DoublyLinkedList{
         this.tail = null;
         this.length = 0;
     }
+    push(val) {
+        let newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length += 1;
+        return this;
+    }
+    pop() {
+        if (this.length === 0) {
+            return null;
+        }
+        let poppedNode = this.tail;
+        this.tail = this.tail.prev;
+        this.tail.next = null;
+        return poppedNode;
+    }
 }
+
+const list = new DoublyLinkedList();
+list.push('one');
+list.push('two');
+list.push('three');
+console.log(list.pop());
 
