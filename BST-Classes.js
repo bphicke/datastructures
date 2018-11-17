ff0610cca5e7ab9596abd6a3bd509184acc62076
brian <bphicke@gmail.com>
2018-11-17 12:39:55 -0600

    Add insert method to Binary Search Tree

--- BST-Classes.js
+++ BST-Classes.js
@@ -10,6 +10,28 @@ class BinarySearchTree {
     constructor(){
         this.root = null;
     }
+    insert(val) {
+        if (!this.root) {
+            this.root = newNode(val);
+            return this;
+        }
+        let current = this.root;
+        while (true) {
+            if (val > current.value) {
+                if (!current.right) {
+                    current.right = new Node(val);
+                    return this;
+                }
+                current = current.right;
+            } else {
+                if (!current.left) {
+                    current.left = new Node(val);
+                    return this;
+                }
+                current = current.left;
+            }
+        }
+    }
 }
 
 var tree = new BinarySearchTree();
@@ -17,3 +39,4 @@ tree.root = new Node(10);
 tree.root.right = new Node(15);
 tree.root.left = new Node(7);
 tree.root.left.right = new Node(9);
+console.log(tree.insert(13));
